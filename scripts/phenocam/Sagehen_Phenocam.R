750750ff3e9b68b018baf563746d18c198ef715d
ShiraGold12 <sgoldman@smith.edu>
2024-07-08 16:27:03 -0700

    Update Sagehen_Phenocam.R
    Changed the drawROI code so that it works for my computer. @jnatali let me know if it works? 
    
    Signed-off-by: ShiraGold12 <sgoldman@smith.edu>

--- scripts/phenocam/Sagehen_Phenocam.R
+++ scripts/phenocam/Sagehen_Phenocam.R
@@ -1,85 +1,70 @@
 library(phenopix)
-setwd("F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/RGB")
+library(raster)
 
-#Create folders for different outputs
-my.path <- structureFolder(path=getwd(),showWarnings=TRUE)
+# Wrapper function -- DrawMULTIROI kept giving me error in brick function
+DrawMULTIROI_Wrapper <- function(path_img_ref, path_ROIs, nroi = 1, roi.names = NULL, file.type = ".jpg") {
+  if (is.null(roi.names)) 
+    roi.names <- paste0("roi", 1:nroi)
+  files <- list.files(path = path_img_ref, pattern = file.type, full.names = TRUE)
+  if (length(files) == 0) {
+    stop("No files found in the specified directory with the given file type.")
+  }
+  
+  roi.data <- list()
+  for (file in files) {
+    img <- brick(file)
+    for (i in seq(as.numeric(nroi))) {
+      plotRGB(img)
+      mtext(paste("ROI ", i, " - ", roi.names[i], " \n a) n left mouse button clicks on ROI vertexes (n>=3) \n b) 1 right mouse button click to close the polygon", 
+                  sep = ""), side = 3, line = -5)
+      answer <- "n"
+      pol.all <- NULL
+      while (answer == "n") {
+        pol1 <- drawPoly(sp = TRUE, col = "red", lwd = 2)
+        answer <- ask("are you done with your roi?\ntype y or n")
+        pol.all <- c(pol.all, pol1)
+      }
+      if (length(pol.all) == 1) 
+        vertices <- pol.all[[1]]
+      else {
+        vertices <- do.call(bind, pol.all)
+      }
+      white.mask <- img[[1]]
+      raster::values(white.mask) <- 1
+      masked.img <- mask(white.mask, vertices)
+      raster::values(masked.img)[is.na(values(masked.img))] <- 0
+      coordinates <- data.frame(rowpos = coordinates(vertices)[, 
+                                                               1], colpos = coordinates(vertices)[, 2])
+      dev.print(jpeg, file = paste(path_ROIs, "/ROI", i, "_", 
+                                   roi.names[i], ".jpg", sep = ""), width = 1024, height = 1024)
+      dev.off()
+      out <- c(masked.img, vertices)
+      names(out) <- c("mask", "polygons")
+      roi.data[[i]] <- out
+    }
+  }
+  names(roi.data) <- roi.names
+  save(roi.data, file = paste(path_ROIs, "roi.data.Rdata", 
+                              sep = ""))
+  return(invisible(roi.data))
+}
 
-path_img_ref="F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/RGB/REF/"
-roi.path="F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/RGB/ROI/"
-img.path="F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/RGB/IMG/"
-vi.path="F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/RGB/VI/"
+# Use the wrapper function
+setwd("/Users/play/Desktop/phenocam/phenopix/RGB")
 
-#Create a ROI for Pepperweed
-windows()
-DrawMULTIROI(path_img_ref = path_img_ref,path_ROIs = roi.path, 
-             nroi=1,file.type=".jpg")
-load('ROI/roi.data.Rdata')
+# Create folders for different outputs
+my.path <- structureFolder(path=getwd(), showWarnings=TRUE)
 
-#extract Vegetation Indices
-extractVIs(img.path=img.path,roi.path=roi.path,vi.path=vi.path,plot=TRUE,begin="2018-01-01",
-           date.code="yyyy_mm_dd_HHMM",file.type=".jpg",npixels=3)
+path_img_ref <- "/Users/play/Desktop/phenocam/phenopix/RGB/REF/RENAMED"
+roi.path <- "/Users/play/Desktop/phenocam/phenopix/RGB/ROI/"
+img.path <- "/Users/play/Desktop/phenocam/phenopix/RGB/IMG/"
+vi.path <- "/Users/play/Desktop/phenocam/phenopix/RGB/VI/"
 
-load('VI/2018-01-01_2019-08-01_VI.data.Rdata')
-names(VI.data[[1]])
+# Create ROI(s) for each meadow
+x11()
+DrawMULTIROI_Wrapper(path_img_ref = path_img_ref, path_ROIs = roi.path, 
+                     nroi = 1, file.type = ".jpg")
 
-#Autofilter
-df=as.data.frame(VI.data$roi1) #creates a dataframe from vegetation indices
-filtered.data <- autoFilter(df,filter=c('night','max'),plot=TRUE,na.fill=TRUE) #removes noise in vegetation indices
-#Night filter removes value below gcc of 0.2
-#Max filter computes the 90% of the curve based on a 3-day moving window (following Sonnetag et al. 2012)
-
-#Fit spline
-greenProcess(filtered.data$max.filtered,'spline','trs',plot=TRUE)
-
-#Convert inputs into dataframe
-library(zoo)
-df=fortify.zoo(filtered.data)
-df$Index=as.Date(df$Index,format="%yyyy/%mm/%dd")
-plot.ts(df$Index,df$max.filtered)
-
-#Plot data
-library(reshape)
-df=read.csv("FilteredData.csv",sep=",")
-df.2017=subset(df,df$Year==2017)
-
-windows()
-par(mfrow=c(4,1),mar=c(3,4,1,1))
-plot(df.2017$DOY,df.2017$bcc,pch=16,col="royalblue3",xlab="DOY",ylab="BCC")
-plot(df.2017$DOY,df.2017$max.filtered,pch=16,col="springgreen3",xlab="DOY",ylab="GCC")
-plot(df.2017$DOY,df.2017$rcc,pch=16,col="red3",xlab="DOY",ylab="RCC")
-plot(df.2017$DOY,df.2017$grR,pch=16,col="black",xlab="DOY",ylab="Green-Red Ratio")
-
-#2018
-df.2018=subset(df,df$Year==2018)
-
-windows()
-par(mfrow=c(4,1),mar=c(3,4,1,1))
-plot(df.2018$DOY,df.2018$bcc,pch=16,col="royalblue3",xlab="DOY",ylab="BCC")
-plot(df.2018$DOY,df.2018$max.filtered,pch=16,col="springgreen3",xlab="DOY",ylab="GCC")
-plot(df.2018$DOY,df.2018$rcc,pch=16,col="red3",xlab="DOY",ylab="RCC")
-plot(df.2018$DOY,df.2018$grR,pch=16,col="black",xlab="DOY",ylab="Green-Red Ratio")
-
-
-# Second focus on 2018 data, with multiple ROIs ---------------------------
-library(phenopix)
-setwd("F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/2018")
-
-#Create folders for different outputs
-my.path <- structureFolder(path=getwd(),showWarnings=TRUE)
-
-path_img_ref="F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/2018/REF/"
-roi.path="F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/2018/ROI/"
-img.path="F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/2018/IMG/"
-vi.path="F:/PostDoc/Data/Preliminary Analysis/RushRanch/Phenocam/2018/VI/"
-
-#Create ROIs
-windows()
-DrawMULTIROI(path_img_ref = path_img_ref,path_ROIs = roi.path, 
-             nroi=1,roi.names=c("Grass"),file.type=".jpg")
-load('ROI/roi.data.Rdata')
-
-extractVIs(img.path=img.path,roi.path=roi.path,vi.path=vi.path,plot=TRUE,
-           date.code="yyyy_mm_dd_HHMM",file.type=".jpg",npixels=3)
-
-load('VI/VI.data.Rdata')
-write.csv(VI.data$Tule,"PhenoData_Tule.csv")
+# Load the ROI data
+roi_data_path <- file.path(roi.path, "roi.data.Rdata")
+load(roi_data_path)
