375bdeddbb1f9ce49312486f1f288876155fd509
ShiraGold12 <sgoldman@smith.edu>
2024-07-22 17:24:04 -0700

    Update Sagehen_Phenocam.R
    Stuck from line 86
    
    Signed-off-by: ShiraGold12 <sgoldman@smith.edu>

--- scripts/phenocam/Sagehen_Phenocam.R
+++ scripts/phenocam/Sagehen_Phenocam.R
@@ -61,10 +61,67 @@ setwd("/Users/play/Documents/GitHub/sagehen_meadows/data/phenocam/phenopix/RGB")
 # Create folders for different outputs
 my.path <- structureFolder(path = getwd(), showWarnings = TRUE)
 
-path_img_ref <- "/Users/play/Documents/GitHub/sagehen_meadows/data/phenocam/phenopix/RGB/REF/RENAMED"
+path_img_ref <- "/Users/play/Documents/GitHub/sagehen_meadows/data/phenocam/phenopix/RGB/REF"
 roi.path <- "/Users/play/Documents/GitHub/sagehen_meadows/data/phenocam/phenopix/RGB/ROI"
-img.path <- "/Users/play/Documents/GitHub/sagehen_meadows/data/phenocam/phenopix/RGB/IMG/RENAMED"
+img.path <- "/Users/play/Documents/GitHub/sagehen_meadows/data/phenocam/phenopix/RGB/IMG"
 vi.path <- "/Users/play/Documents/GitHub/sagehen_meadows/data/phenocam/phenopix/RGB/VI"
 
 # Create ROI(s) for each meadow
 DrawMULTIROI_Wrapper(path_img_ref = path_img_ref, path_ROIs = roi.path, nroi = 3, file.type = ".jpg")
+
+load('ROI/roi.data.Rdata')
+
+
+#EDIT BELOW
+
+load("/Users/play/Documents/GitHub/sagehen_meadows/data/phenocam/phenopix/RGB/VI/VI.data.Rdata")
+
+
+class(VI.data[[1]])
+
+head(names(VI.data[[1]]))
+
+str(VI.data[[1]][[1]])
+
+#having trouble here with filtering data... the rest is example and unedited
+
+
+#Autofilter
+df=as.data.frame(VI.data$roi1) #creates a dataframe from vegetation indices
+names(df)
+
+filtered.data <- autoFilter(df,filter=c('night','max'),plot=TRUE,na.fill=TRUE) #removes noise in vegetation indices
+#Night filter removes value below gcc of 0.2
+#Max filter computes the 90% of the curve based on a 3-day moving window (following Sonnetag et al. 2012)
+
+
+#Fit spline
+greenProcess(filtered.data$max.filtered,'spline','trs',plot=TRUE)
+
+#Convert inputs into dataframe
+library(zoo)
+df=fortify.zoo(filtered.data)
+df$Index=as.Date(df$Index,format="%yyyy/%mm/%dd")
+plot.ts(df$Index,df$max.filtered)
+
+#Plot data
+library(reshape)
+df=read.csv("FilteredData.csv",sep=",")
+df.2017=subset(df,df$Year==2017)
+
+windows()
+par(mfrow=c(4,1),mar=c(3,4,1,1))
+plot(df.2017$DOY,df.2017$bcc,pch=16,col="royalblue3",xlab="DOY",ylab="BCC")
+plot(df.2017$DOY,df.2017$max.filtered,pch=16,col="springgreen3",xlab="DOY",ylab="GCC")
+plot(df.2017$DOY,df.2017$rcc,pch=16,col="red3",xlab="DOY",ylab="RCC")
+plot(df.2017$DOY,df.2017$grR,pch=16,col="black",xlab="DOY",ylab="Green-Red Ratio")
+
+#2018
+df.2018=subset(df,df$Year==2018)
+
+windows()
+par(mfrow=c(4,1),mar=c(3,4,1,1))
+plot(df.2018$DOY,df.2018$bcc,pch=16,col="royalblue3",xlab="DOY",ylab="BCC")
+plot(df.2018$DOY,df.2018$max.filtered,pch=16,col="springgreen3",xlab="DOY",ylab="GCC")
+plot(df.2018$DOY,df.2018$rcc,pch=16,col="red3",xlab="DOY",ylab="RCC")
+plot(df.2018$DOY,df.2018$grR,pch=16,col="black",xlab="DOY",ylab="Green-Red Ratio")
