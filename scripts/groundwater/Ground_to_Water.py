57237c72e182bb791f777ff9a6d83aa0e8413b0b
Jennifer Natali <jennifer.natali@gmail.com>
2024-10-03 17:15:10 -0700

    Stubbed out code blocks for Ground_to_Water.py + more data validation
    -[] In Ground_to_Water.py, tested data fetching and dataframe setup from files
    -[] In Ground_to_Water.py, stubbed out steps to determine welltop_to_ground (or an estimate based on well depth)
    -[] Validated well_ids in well_dimensions csv

--- scripts/groundwater/Ground_to_Water.py
+++ scripts/groundwater/Ground_to_Water.py
@@ -15,9 +15,10 @@ Major Functions:
 TRANSLATES 
 
 Requires X data files:
-    1. 
-    2. 
-    3. 
+1. Unique well ids: 'well_unique_id.txt'
+2. Well Dimensions: 'well_dimensions.csv'
+3. RAW groundwater data (in cm) for all years: 'groundwater_biweekly_RAW.csv'
+4. Well meter offsets (in cm): 'well_meter_offsets.csv'
 
 TODOs documented in github repo issue tracking.
 
@@ -44,27 +45,59 @@ from datetime import datetime
 gw_data_dir = '../../data/field_observations/groundwater/biweekly_manual/'
 gw_plot_dir = '../../data/field_observations/groundwater/plots/biweekly_manual/'
 
-# Input (source data) files
-gw_in_filename = gw_data_dir + 'groundwater_biweekly_RAW.csv'
-well_dimension_filename = gw_data_dir + 'well_dimensions.csv'
-meter_offset_filename = gw_data_dir + 'well_dimensions.csv'
+# Input (source data) filenames
+well_unique_id_filename = gw_data_dir + '../well_unique_id.txt'
+well_dimension_filename = gw_data_dir + '../well_dimensions.csv'
+meter_offset_filename = gw_data_dir + '../well_meter_offsets.csv'
+gw_rawdata_filename = gw_data_dir + 'groundwater_biweekly_RAW.csv'
 
-# Output files
-gw_out_filename = gw_data_dir + 'groundwater_biweekly_FULL.csv'
+# Output filenames
+gw_fulldata_filename = gw_data_dir + 'groundwater_biweekly_FULL.csv'
 gw_plot_filename = gw_plot_dir + 'groundwater_biweekly_2018-2021.pdf'
 
+# Fetch input (source data) files
+well_unique_id = pd.read_csv(well_unique_id_filename)
+well_dimension = pd.read_csv(well_dimension_filename)
+gw_rawdata = pd.read_csv(gw_rawdata_filename)
+meter_offset = pd.read_csv(meter_offset_filename)
+
+# --- VALIDATE well_id's and well_dimension.welltop_to_ground ---
+# 
+# 1. Check that all well_id's in gw_rawdata and well_dimension are
+#    found in well_unique_id; if not, print in a WARNING and save in a CSV.
+#
+# 2. Check that every well_id in gw_rawdata has a matching welltop_to_ground;
+#    if not, print in a WARNING and save well_id and date in a CSV.
+
+# --- DETERMINE welltop_to_ground_cm ---
+#
+# 1. For each well reading, subtract well_dimension.welltop_to_ground_cm 
+#    from gw_rawdata.welltop_to_water_cm for the matching well_id
+#
+# 2. For each reading, apply the appropriate meter offset based on
+#    gw_rawdata.meter_id
+#
+# 3. If water_binary is false (no water), estimate the welltop_to_ground
+#    as "greater than the" (total_well_length_cm - welltop_to_ground_cm)
+#    and set as welltop_to_ground_gt_cm.
+#
+# 4. Save gw_fulldata with the adjusted welltop_to_ground_cm and 
+#    welltop_to_ground_gt_cm measurements along with following params 
+#    from gw_rawdata: well_id, timestamp,
+#    welltop_to_water_cm, water_binary, logger_binary, meter_id,
+#    soil_moisture, notes
 
 
 
-# --- UTILITY FUNCTIONS ---
-def SAMPLE_get_poly_area(x, y) -> float:
-    """
-    Calculate area of a polygon.
+# # --- UTILITY FUNCTIONS ---
+# def SAMPLE_get_poly_area(x, y) -> float:
+#     """
+#     Calculate area of a polygon.
 
-    Parameters:
-    x (array of float): series of x coordinates
-    y (array of float): series of y coordinates
+#     Parameters:
+#     x (array of float): series of x coordinates
+#     y (array of float): series of y coordinates
 
-    Returns:
-    a_np (float): the area of polygon
-    """
+#     Returns:
+#     a_np (float): the area of polygon
+#     """
