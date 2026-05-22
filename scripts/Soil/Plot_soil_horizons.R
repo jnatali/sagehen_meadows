a31fee66b4ed29a0da3cb9de71a742fa5a73cd98
KaraSmit <smittlekara@gmail.com>
2026-05-21 20:33:27 -0700

    Updated to include function
    Signed-off-by: KaraSmit <smittlekara@gmail.com>

--- scripts/Soil/Plot_soil_horizons.R
+++ scripts/Soil/Plot_soil_horizons.R
@@ -1,58 +1,45 @@
-# Load the required package
+
+# install.packages("dplyr")
+# install.packages("stringr")
+
 library(aqp)
+library(dplyr)
+library(stringr)
 
-# Load the CSV file
-soil_df <- read.csv("C:/Users/smitt/Downloads/soil_survey_at_wells_update_w_G.csv")
+# Setup 
+# Make sure this path points exactly to where well_utils.R is saved
+source("scripts/groundwater/well_utils.R")
 
-# Convert to SoilProfileCollection (Replace with your actual column names!)
-depths(soil_df) <- well_id ~ start_depth_cm + stop_depth_cm
+# Load data
+df <- read.csv("data/field_observation/soil/Cleaned_wells_complete.csv")
 
-soil_df$gravel_amount_percent_hundred <- soil_df$gravel_amount_percent * 100
+# Add categories 
+df <- get_well_categories(df)
 
-# Filter the data into the three groups
-wells_F <- subset(soil_df, grepl("F", well_id))
-wells_R <- subset(soil_df, grepl("R", well_id))
-wells_T <- subset(soil_df, grepl("T", well_id))
+#  Filter a specific category
+east_meadow_df <- df %>%
+  filter(hydrogeo_zone == "Fan")
 
+# Convert the filtered data to a SoilProfileCollection
+depths(east_meadow_df) <- well_id ~ start_depth_cm + stop_depth_cm
 
-# Create and save the plots to a specified folder
+# Add the gravel calculation for the plot bubbles
+east_meadow_df$gravel_amount_percent_hundred <- east_meadow_df$gravel_amount_percent * 100
 
-#  Save Wells containing 'F' 
-png(filename = "D:/Research_Jen/Soil_survey/Graphs/wells_Fan_plot.png", width = 1600, height = 1200, res = 150)
-plotSPC(wells_F, 
-        name = 'soil_texture_code',          
-	color = 'gravel_amount_percent',  
-        label = 'well_id')        
-addVolumeFraction(wells_F, colname = 'gravel_amount_percent_hundred', res = 10,
-  cex.min = 0.1,
-  cex.max = 0.5,
-  pch = 1,
-  col = "black")
-dev.off()
-
-# Save Wells containing 'R' 
-png(filename = "D:/Research_Jen/Soil_survey/Graphs/wells_Riparian_plot.png", width = 1600, height = 1200, res = 150)
-plotSPC(wells_R, 
-        name = 'soil_texture_code',  
-        color = 'gravel_amount_percent',  
-        label = 'well_id') 
-addVolumeFraction(wells_R, colname = 'gravel_amount_percent_hundred', res = 10,
-  cex.min = 0.1,
-  cex.max = 0.5,
-  pch = 1,
-  col = "black")
-dev.off()
+# Plot and Save
+png(filename = "results/plots/groundwater/soils/wells_Fan_HydroZone.png", 
+    width = 1600, height = 1200, res = 150)
 
 
-# Save Wells containing 'T' ---
-png(filename = "D:/Research_Jen/Soil_survey/Graphs/wells_Terrace_plot.png", width = 1600, height = 1200, res = 150)
-plotSPC(wells_T, 
-        name = 'soil_texture_code',  
-        color = 'gravel_amount_percent',  
-        label = 'well_id') 
-addVolumeFraction(wells_T, colname = 'gravel_amount_percent_hundred', res = 10,
-  cex.min = 0.1,
-  cex.max = 0.5,
-  pch = 1,
-  col = "black")
+
+plotSPC(east_meadow_df, 
+        name = 'soil_texture_code', 
+        color = 'gravel_amount_percent', 
+        label = 'well_id')
+
+addVolumeFraction(east_meadow_df, 
+                  colname = 'gravel_amount_percent_hundred', 
+                  res = 10, cex.min = 0.1, cex.max = 0.5, 
+                  pch = 1, col = "black")
+
 dev.off()
