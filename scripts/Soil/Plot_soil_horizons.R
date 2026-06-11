828c978daf4f10dd20b08dbd50e6e44239648c74
KaraSmit <smittlekara@gmail.com>
2026-06-10 21:28:05 -0700

    Updating plots

--- scripts/Soil/Plot_soil_horizons.R
+++ scripts/Soil/Plot_soil_horizons.R
@@ -17,9 +17,13 @@ df <- read.csv("data/field_observations/soil/soil_survey_COMPLETE.csv")
 # Add categories 
 df <- get_well_categories(df)
 
+df$start_depth_cm <- round(df$start_depth_cm)
+df$stop_depth_cm <- round(df$stop_depth_cm)
+
 #  Filter a specific category
 east_meadow_df <- df %>%
   filter(hydrogeo_zone == "Fan")
+
 
 # Convert the filtered data to a SoilProfileCollection
 depths(east_meadow_df) <- well_id ~ start_depth_cm + stop_depth_cm
@@ -29,21 +33,24 @@ east_meadow_df$gravel_amount_percent_hundred <- east_meadow_df$gravel_amount_per
 
 # Plot and Save
 png(filename = "results/plots/groundwater/soils/wells_Fan_HydroZone_TEST.png", 
-    width = 1600, height = 1200, res = 150)
+    width = 1800, height = 2000, res = 150)
 
 
-
+par(mar = c(0, 0, 1, 1))
 plotSPC(east_meadow_df, 
-        name = 'soil_texture_code',
-        style = 'center',
+        cex.name = 1,
+        name.style = 'center-center',
+        width = 0.3,
+        depth.axis = FALSE,
         color = 'soil_texture_code', 
         hz.depths = TRUE,
-        fixLabelCollisions = TRUE
+        fixLabelCollisions = TRUE,
+       depths.offset = 0.08
         )
 
 addVolumeFraction(east_meadow_df, 
                   colname = 'gravel_amount_percent_hundred', 
                   res = 10, cex.min = 0.1, cex.max = 0.5, 
-                  pch = 1, col = "black")
+                  pch = 1, col = "white")
 
 dev.off()
